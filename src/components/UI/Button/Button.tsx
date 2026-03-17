888ea8f198eccfaf83a7225dee8f9aa169d30968
Albert Arakelyan <albertarakelyan1998@gmail.com>
2026-03-17 22:48:35 +0400

    feat(25): small corrections on Button component

--- src/components/UI/Button/Button.tsx
+++ src/components/UI/Button/Button.tsx
@@ -31,11 +31,11 @@ const Button: FC<IButtonProps> = ({
     const variantMapping: Record<ButtonVariantType, string> = {
       primary: 'bg-primary hover:bg-primary-hover active:bg-primary-active text-white',
       secondary: 'bg-secondary hover:bg-secondary-hover active:bg-secondary-active text-white',
+      ghost: 'bg-transparent hover:bg-ghost-button-hover-gray-bg active:bg-ghost-button-active-gray-bg text-text-color', // add hover and active colors to base.css and tailwind.config.js
       danger: 'bg-danger hover:bg-danger-hover active:bg-danger-active text-white', // TODO: add hover and active colors to base.css and tailwind.config.js
       success: 'bg-success hover:bg-success-hover active:bg-success-active text-white', // TODO: add hover and active colors to base.css and tailwind.config.js
       warning: 'bg-warning hover:bg-warning-hover active:bg-warning-active text-white', // TODO: add hover and active colors to base.css and tailwind.config.js
       info: 'bg-info hover:bg-info-hover active:bg-info-active text-white', // TODO: add hover and active colors to base.css and tailwind.config.js
-      ghost: 'bg-transparent hover:ghost-button-hover-gray-bg active:ghost-button-active-gray-bg text-text-color', // add hover and active colors to base.css and tailwind.config.js
     };
 
     return variantMapping[variant] || variantMapping.primary;
