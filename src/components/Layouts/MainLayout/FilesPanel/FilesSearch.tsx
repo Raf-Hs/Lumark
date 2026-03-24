90c4b5373b8c711e0908e346acd9a88d555d5a65
Albert Arakelyan <albertarakelyan1998@gmail.com>
2026-03-24 22:43:27 +0400

    feat(33): created file create input structure

--- src/components/Layouts/MainLayout/FilesPanel/FilesSearch.tsx
+++ src/components/Layouts/MainLayout/FilesPanel/FilesSearch.tsx
@@ -1,15 +1,54 @@
+import { useState } from 'react';
 import { SquarePenIcon } from 'lucide-react';
 import Button from '../../../UI/Button/Button';
 import Input from '../../../UI/Input/Input';
 
-const FilesSearch = () => (
-  <div className="flex items-center gap-1">
-    <Input
-      placeholder="Search"
-      rounded="rounded"
-    />
-    <Button variant="ghost" size="square-icon" icon={<SquarePenIcon />} />
-  </div>
-);
+const FilesSearch = () => {
+  const [isAddingFile, setIsAddingFile] = useState(false);
+
+  const handleAddFileClick = () => {
+    setIsAddingFile(true);
+  };
+
+  return (
+    isAddingFile ? (
+      <div className="flex flex-col items-start gap-1">
+        <Input
+          key="file-name-input"
+          placeholder="File name"
+          rounded="rounded"
+        />
+        <div className="flex items-center gap-1 w-full justify-end">
+          <Button
+            size="sm"
+            variant="success"
+          >
+            Add
+          </Button>
+          <Button
+            size="sm"
+            variant="danger"
+          >
+            Cancel
+          </Button>
+        </div>
+      </div>
+    ) : (
+      <div className="flex items-center gap-1">
+        <Input
+          key="file-search-input"
+          placeholder="Search"
+          rounded="rounded"
+        />
+        <Button
+          variant="ghost"
+          size="square-icon"
+          icon={<SquarePenIcon />}
+          onClick={handleAddFileClick}
+        />
+      </div>
+    )
+  );
+};
 
 export default FilesSearch;
