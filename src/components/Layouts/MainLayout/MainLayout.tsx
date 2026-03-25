b91d11f87df64e84091afe4555cdb665445bf789
Albert Arakelyan <albertarakelyan1998@gmail.com>
2026-03-25 12:48:45 +0400

    fix(33): did small type improvement on MainLayout.tsx

--- src/components/Layouts/MainLayout/MainLayout.tsx
+++ src/components/Layouts/MainLayout/MainLayout.tsx
@@ -1,8 +1,7 @@
-import { FC } from 'react';
-import { IMainLayoutProps } from './types';
+import { FC, PropsWithChildren } from 'react';
 import FilesPanel from './FilesPanel/FilesPanel';
 
-const MainLayout: FC<IMainLayoutProps> = ({ children }) => (
+const MainLayout: FC<PropsWithChildren> = ({ children }) => (
   <div className="flex items-start">
     <aside className="max-w-64 w-full p-2 border-r border-border-color min-h-screen sticky top-0">
       <FilesPanel />
--- src/components/Layouts/MainLayout/types.ts
+++
@@ -1,3 +0,0 @@
-import { PropsWithChildren } from 'react';
-
-export interface IMainLayoutProps extends PropsWithChildren {}
